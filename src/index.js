11eff5321e40bccdc0ee80982b025f11030e2f73
Ryan <draco124@hotmail.com>
2021-07-06 11:35:22 -0700

    1

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import ReactDOM from "react-dom";
 import "./App.css";
 // import App from './App';
 
-function Tbl({ incell, id, getinfo, ref }) {
-  const [arr, setArr] = useState(Array(12).fill(Array(3).fill(null)));
-
+function Tbl({ incell, id, getinfo, ref, arr }) {
+  // const [arr, setArr] = useState(Array(12).fill(Array(3).fill(null)));
   let i = 0;
   // React.useEffect(() => {
   //   console.log("change");
   // });
-  // console.log(arr.length);
+  console.log(arr);
   // document.querySelector("#a3").innerText
-  console.log(ref);
+  // console.log(ref);
   return (
     <table id={id}>
       <tbody>
+        {console.log(arr)}
         {arr.map((e) => (
-          <Roww arr={arr[0]} i={(i += 3)} incell={incell} />
+          <Roww arr2={arr[0]} i={(i += 3)} incell={incell} />
         ))}
       </tbody>
     </table>
   );
 }
-function Roww({ arr, i, incell, getinfo, ref }) {
+function Roww({ arr2, i, incell, getinfo, ref }) {
   // console.log(arr[0].length)
   return (
     <tr>
-      {arr.map((e) => (
+      {arr2.map((e) => (
         <Cell incell={incell} i={i++} />
       ))}
     </tr>
@@ -45,20 +45,19 @@ function Cell({ i, incell, getinfo, ref }) {
   );
 }
 function Temp() {
+  const [arr, setArr] = useState(Array(12).fill(Array(3).fill(0)));
   const cellRef = useRef();
   return (
     <>
       <h1>Hello World</h1>
-      <Tbl id={"tbl" + String(0)} incell={"asd"} getinfo={null} />
+      <Tbl id={"tbl" + String(0)} incell={"asd"} getinfo={null} arr={arr} />
       <Tbl
         id={"tbl" + String(1)}
         incell={<input type="number" ref={cellRef} />}
+        arr={arr}
+      />
       />
     </>
   );
 }
 ReactDOM.render(<Temp />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
